cf52722cf02869d908c5a47aca287604c951e7d4
AndresAndrada <andresandrada1994@gmail.com>
2024-08-05 22:28:10 -0300

    Add chack auth

--- src/routers/userRouter/userRouter.js
+++ src/routers/userRouter/userRouter.js
@@ -2,12 +2,13 @@ const Router = require('express');
 const { getAllUser } = require('../../controllers/user/getAllUser');
 const { createUser } = require('../../controllers/user/createUser');
 const { login } = require('../../controllers/user/login');
+const { checkAuth } = require('../../auth/checkAuth');
 // const { registerProduct } = require('../../controllers/product/registerProduct');
 
 const userRouter = Router();
 
 // GET
-userRouter.get('/', getAllUser);
+userRouter.get('/', checkAuth, getAllUser);
 
 // POST
 userRouter.post('/', createUser);
