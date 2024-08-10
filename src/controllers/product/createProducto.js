bc16a6aa7c6e070ec1ba4736aca2d2d1abd3883a
AndresAndrada <andresandrada1994@gmail.com>
2024-08-09 21:51:37 -0300

    Change create product

--- src/controllers/product/createProducto.js
+++ src/controllers/product/createProducto.js
@@ -1,8 +1,11 @@
 const { Product } = require("../../db/db");
 
 const createProduct = async (req, res) => {
+    const { name_produc } = req.query;
     const product = req.body;
     try {
+        const productFind = await Product.findOne({ where: name_produc })
+        console.log(productFind.id, 'FIND');
         const newProducto = await Product.create(product);
         res.send({ newProducto, message: 'Create product succesfull' });
     } catch (error) {
