ecb2f613a89783ac21d6839e3ab4957ea483023b
AndresAndrada <andresandrada1994@gmail.com>
2024-08-21 12:18:53 -0300

    Add auth admin

--- src/middleware/checkRoleAuth.js
+++ src/middleware/checkRoleAuth.js
@@ -3,11 +3,11 @@ const { verifyToken } = require("./verifyToken");
 
 const chackRoleAuth = (roles) => async (req, res, next) => {
     try {
-        const token = req.headers.authorization ? req.headers.authorization.split().pop() : undefined;
-        const tokenData = await verifyToken(token)
+        const token = req.headers.authorization ? req.headers.authorization.split(' ').pop() : undefined;
+        const tokenData = token ? await verifyToken(token) : undefined;
         const userData = await User.findByPk(tokenData._id);
-        console.log(tokenData, 'TOKEN DATA');
-        userData.admin ? next() : res.status(409).send({ message: 'Token is not define' })
+        console.log(userData.admin, 'TOEN DATA');
+        userData.admin ? next() : res.status(409).send({ message: "You don't have authorization" })
     } catch (error) {
         res.status(304).send({ message: error.message });
     }
--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = async (token) => {
     try {
+        // console.log(token, 'TOKEN VERIFI');
         return jwt.verify(token, process.env.JWT_SECRET)
     } catch (error) {
-        console.error({ error: error.message });
+        console.error({ message: error.message });
     }
 }
 
