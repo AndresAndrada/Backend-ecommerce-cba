7855ab5852d38eb951903b2d450727919c020f0c
AndresAndrada <andresandrada1994@gmail.com>
2024-09-29 12:14:13 -0300

    Change the patch product image

--- src/controllers/product/patchImageProducto.js
+++ src/controllers/product/patchImageProducto.js
@@ -3,14 +3,13 @@ const { cloudinaryPost } = require("../../utils/cloudinary");
 
 const patchImageProduct = async (req, res) => {
     const { idProduct } = req.params;
-    const { image } = req.files;
     try {
         const productFind = await Product.findByPk(idProduct);
-        if (productFind.dataValues) {
-            const img = await cloudinaryPost(image.tempFilePath);
+        if (productFind.dataValues && req.files?.image) {
+            const img = await cloudinaryPost(req.files?.image.tempFilePath);
             const userImage = await Product.update({ image: img }, { where: { id: idProduct } });
-            res.send({ userImage, message: 'Modified already exists' });
-        } else res.status(309).send({ message: 'Product all exist' })
+            res.send({ userImage, message: 'Product modified' });
+        } else res.send({ message: 'Something went wrong' })
     } catch (error) {
         res.status(304).send({ message: error.message });
     };
