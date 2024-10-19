3aa84d28d7305e0428a998951f505f351694a226
AndresAndrada <andresandrada1994@gmail.com>
2024-10-18 23:26:16 -0300

    Change the patch image product

--- src/controllers/product/deleteProduct.js
+++ src/controllers/product/deleteProduct.js
@@ -4,17 +4,13 @@ const deleteProduct = async (req, res) => {
     const { idProduct } = req.params;
     const { logicalDelete } = req.body;
     try {
-        try {
-            const deleteProduct = !logicalDelete
-                ? await Product.destroy({ where: { id: idProduct } })
-                : await Product.update({ status: false }, { where: { id: idProduct } });
-            if (!deleteProduct) throw new Error("Resquest failed.");
-            res.send({ message: 'Producto eliminado' });
-        } catch (error) {
-            throw error;
-        }
+        const deleteProduct = !logicalDelete
+            ? await Product.destroy({ where: { id: idProduct } })
+            : await Product.update({ status: false }, { where: { id: idProduct } });
+        if (!deleteProduct) throw new Error("Resquest failed.");
+        res.send({ message: 'Producto eliminado' });
     } catch (error) {
-        res.status(304).send({ message: error.message });
+        res.status(304).send({ message: error });
     };
 };
 
